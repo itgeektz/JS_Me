0f4dafc23f5ed78de0becd4b3cda9dd796bb1819
itgeektz <nidhin.regency@gmail.com>
2024-06-22 12:04:43 -0400

    reset to followers

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -13,9 +13,15 @@ document.getElementById('countDisplay').innerHTML=count; // Display the count in
 }
 
 function checkCountValue() {
-  if (count === 10) {
-    alert("Your Instagram post gained 10 followers! Congratulations!");
+  
+  if (count%10 === 0) {
+        alert("Your Instagram post gained "+count+" followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
+}
+
+function reset(){
+    count = 0;
+    displayCount();
 }
