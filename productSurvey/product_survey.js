0169e136b773c40405d7f46a7947eca740c7dfbf
itgeektz <nidhin.regency@gmail.com>
2024-06-23 16:46:41 -0400

    Next change for product feedback

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -4,21 +4,30 @@ function submitFeedback() {
     const email = document.getElementById('email').value;
     const job = document.getElementById('job').value;
     const designation = document.getElementById('designation').value;
+    const Experince=document.getElementById("Experince").value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-    const submitButton=document.getElementById('submitBtn');
-    submitButton.onclick = submitFeedback;
-    alert('Thank you for your valuable feedback')
+   
+    
+    
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
     document.getElementById('userJob').innerHTML = job;
     document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById("userExperince").innerHTML = Experince;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
     document.getElementById('userInfo').style.display = 'block';
+    
+    alert('Thank you for your valuable feedback')
 }
+
+const submitButton=document.getElementById('submitBtn');
+submitButton.onclick = submitFeedback;
+    
+
 
 document.addEventListener('keydown', function(event) {
    if (event.key === 'Enter') {
